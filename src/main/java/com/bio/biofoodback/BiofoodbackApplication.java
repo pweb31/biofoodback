857f70b48a776f1bd522a5c5dce96e5d5a6b5ab0
LELO <nyaka.lelo@econocom.com>
2020-07-11 14:17:17 +0200

    initial commit

---
+++ src/main/java/com/bio/biofoodback/BiofoodbackApplication.java
@@ -0,0 +1,13 @@
+package com.bio.biofoodback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiofoodbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiofoodbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bio/biofoodback/BiofoodbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bio.biofoodback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiofoodbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
