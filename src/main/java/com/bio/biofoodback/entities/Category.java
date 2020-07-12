d393c07616a4eb295d7aa3128ff7cfea5f6c7315
LELO <nyaka.lelo@econocom.com>
2020-07-12 04:11:25 +0200

    Ajout entites java spring boot

---
+++ src/main/java/com/bio/biofoodback/entities/Category.java
@@ -0,0 +1,98 @@
+package com.bio.biofoodback.entities;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Category implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nom;
+	@Lob
+	private byte[] imgCategory;
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Set<Product> products;
+
+	public Category() {
+	}
+
+	public Category(String nom, Set<Product> products) {
+		this.nom = nom;
+		this.products = products;
+	}
+
+	public Category(String nom) {
+		this.nom = nom;
+	}
+
+	@JsonIgnore
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public byte[] getImgCategory() {
+		return imgCategory;
+	}
+
+	public void setImgCategory(byte[] imgCategory) {
+		this.imgCategory = imgCategory;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id != other.id)
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (products == null) {
+			if (other.products != null)
+				return false;
+		} else if (!products.equals(other.products))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/bio/biofoodback/entities/Product.java
@@ -0,0 +1,123 @@
+package com.bio.biofoodback.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Product implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nom;
+	private float prix;
+	private String description;
+	@Column(name = "DERNIER_MAJ")
+	@Temporal(TemporalType.DATE)
+	private Date dernier_maj;
+	@ManyToOne
+	@JoinColumn(name = "CATEGORY_ID")
+	private Category category;
+	@OneToMany(mappedBy = "product", fetch = FetchType.LAZY)
+	private Set<ProductCommandeClient> prodCmdClient = new HashSet<ProductCommandeClient>();
+	@Lob
+	private byte[] imgProduit;
+
+	public Product() {
+	}
+
+	public Product(String nom, float prix, String description, Date dernier_maj) {
+
+		this.nom = nom;
+		this.prix = prix;
+		this.description = description;
+		this.dernier_maj = dernier_maj;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public float getPrix() {
+		return prix;
+	}
+
+	public void setPrix(float prix) {
+		this.prix = prix;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDernier_maj() {
+		return dernier_maj;
+	}
+
+	public void setDernier_maj(Date dernier_maj) {
+		this.dernier_maj = dernier_maj;
+	}
+
+	@JsonIgnore
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@JsonIgnore
+	public Set<ProductCommandeClient> getProdCmdClient() {
+		return prodCmdClient;
+	}
+
+	public void setProdCmdClient(Set<ProductCommandeClient> prodCmdClient) {
+		this.prodCmdClient = prodCmdClient;
+	}
+
+	public byte[] getImgProduit() {
+		return imgProduit;
+	}
+
+	public void setImgProduit(byte[] imgProduit) {
+		this.imgProduit = imgProduit;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
